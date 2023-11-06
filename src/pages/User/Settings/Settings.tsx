eb03a4ffaeac9821f5b945a14e3301dff953c131
Laure Bellanger <5022584-laurebll@users.noreply.gitlab.com>
2023-11-06 05:30:24 +0100

    feat: merge branches

--- src/pages/User/Settings/Settings.tsx
+++ src/pages/User/Settings/Settings.tsx
@@ -19,6 +19,7 @@ import { logout } from "../../../Config/Auth";
 import useCreateAccountStore from "../../../hooks/zustand/CreateAccountStore";
 import { API_URL } from "../../../routes/Url";
 import axios from "axios";
+import Navbar from "../../../components/Navbar";
 
 enum TabEnum {
   Profile,
@@ -86,125 +87,128 @@ const Settings = () => {
   }, [profile]);
 
   return (
-    <Box sx={{ background: COLORS.basicGrey, height: "100vh" }}>
-      <Menu
-        content={
-          <Box>
-            <Typography
-              sx={{
-                color: COLORS.deepBlue,
-                fontSize: "1.8rem",
-                fontWeight: 500,
-                mb: 4,
-              }}
-            >
-              Settings
-            </Typography>
-            <Box
-              sx={{
-                height: "55vh",
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "space-between",
-                gap: "1.5rem",
-              }}
-            >
-              <List>
-                {items.map((item) => (
-                  <ListItem key={item.label}>
-                    <ListItemButton
-                      selected={selectedTab === item.tab}
-                      onClick={() => setSelectedTab(item.tab)}
-                      sx={{
-                        padding: "1rem 1.5rem",
-                        borderRadius: "1rem",
-                        "&.Mui-selected, &:hover": {
-                          background: selectedTab === item.tab ? "#F4E9DE" : "transparent",
-                        },
-                      }}
-                    >
-                      <ListItemIcon sx={{ color: COLORS.deepBlue }}>{item.icon}</ListItemIcon>
-                      <ListItemText
+    <>
+      <Navbar />
+      <Box sx={{ background: COLORS.basicGrey, height: "calc(100vh - 60px)", mt: "60px" }}>
+        <Menu
+          content={
+            <Box sx={{ mt: "60px" }}>
+              <Typography
+                sx={{
+                  color: COLORS.deepBlue,
+                  fontSize: "1.8rem",
+                  fontWeight: 500,
+                  mb: 4,
+                }}
+              >
+                Settings
+              </Typography>
+              <Box
+                sx={{
+                  height: "55vh",
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "space-between",
+                  gap: "1.5rem",
+                }}
+              >
+                <List>
+                  {items.map((item) => (
+                    <ListItem key={item.label}>
+                      <ListItemButton
+                        selected={selectedTab === item.tab}
+                        onClick={() => setSelectedTab(item.tab)}
                         sx={{
-                          color: COLORS.deepBlue,
-                          ".MuiTypography-root": {
-                            fontSize: "1.2rem",
-                            fontWeight: selectedTab === item.tab ? 600 : 400,
+                          padding: "1rem 1.5rem",
+                          borderRadius: "1rem",
+                          "&.Mui-selected, &:hover": {
+                            background: selectedTab === item.tab ? "#F4E9DE" : "transparent",
                           },
                         }}
                       >
-                        {item.label}
+                        <ListItemIcon sx={{ color: COLORS.deepBlue }}>{item.icon}</ListItemIcon>
+                        <ListItemText
+                          sx={{
+                            color: COLORS.deepBlue,
+                            ".MuiTypography-root": {
+                              fontSize: "1.2rem",
+                              fontWeight: selectedTab === item.tab ? 600 : 400,
+                            },
+                          }}
+                        >
+                          {item.label}
+                        </ListItemText>
+                      </ListItemButton>
+                    </ListItem>
+                  ))}
+                </List>
+                <List>
+                  <ListItem sx={{ mb: "2rem" }}>
+                    <ListItemButton
+                      onClick={logout}
+                      sx={{
+                        padding: "1rem 1.5rem",
+                        mb: "2rem",
+                        "&:hover, &:focus": { background: "transparent" },
+                      }}
+                    >
+                      <ListItemIcon sx={{ color: COLORS.deepBlue }}>
+                        <ExitToAppOutlined fontSize="large" />
+                      </ListItemIcon>
+                      <ListItemText
+                        sx={{
+                          color: COLORS.deepBlue,
+                          ".MuiTypography-root": { fontSize: "1.2rem", fontWeight: 500 },
+                        }}
+                      >
+                        Log out
                       </ListItemText>
                     </ListItemButton>
                   </ListItem>
-                ))}
-              </List>
-              <List>
-                <ListItem sx={{ mb: "2rem" }}>
-                  <ListItemButton
-                    onClick={logout}
-                    sx={{
-                      padding: "1rem 1.5rem",
-                      mb: "2rem",
-                      "&:hover, &:focus": { background: "transparent" },
-                    }}
-                  >
-                    <ListItemIcon sx={{ color: COLORS.deepBlue }}>
-                      <ExitToAppOutlined fontSize="large" />
-                    </ListItemIcon>
-                    <ListItemText
-                      sx={{
-                        color: COLORS.deepBlue,
-                        ".MuiTypography-root": { fontSize: "1.2rem", fontWeight: 500 },
-                      }}
-                    >
-                      Log out
-                    </ListItemText>
-                  </ListItemButton>
-                </ListItem>
-              </List>
+                </List>
+              </Box>
             </Box>
-          </Box>
-        }
-      />
-      <Stack
-        direction="row"
-        justifyContent="center"
-        marginLeft={{
-          xs: 0,
-          md: "350px",
-        }}
-      >
-        <Stack direction="column" justifyContent="center" sx={{ py: "2rem" }} gap={"2rem"}>
-          <Box
-            component="h3"
-            sx={{
-              color: COLORS.royablBlue,
-              fontSize: "2.5rem",
-              fontWeight: 600,
-            }}
-          >
-            {selectedTab === TabEnum.Profile ? "Edit your account" : "Order history"}
-          </Box>
-          {selectedTab === TabEnum.Profile && (
-            <>
-              <ProfileForm
-                isLoading={isLoading}
-                allowEmptyPassword
-                displayDeleteAccount
-                submitButton={{
-                  onClick: () => handleSubmit(),
-                  content: "Save",
-                }}
-              />
-              {!!errorMessage && <Alert severity="warning">{errorMessage}</Alert>}
-              {!!successMessage && <Alert severity="success">{successMessage}</Alert>}
-            </>
-          )}
-          {selectedTab === TabEnum.OrderHistory && <Invoices />}
+          }
+        />
+        <Stack
+          direction="row"
+          justifyContent="center"
+          marginLeft={{
+            xs: 0,
+            md: "350px",
+          }}
+        >
+          <Stack direction="column" justifyContent="center" sx={{ py: "2rem" }} gap={"2rem"}>
+            <Box
+              component="h3"
+              sx={{
+                color: COLORS.royablBlue,
+                fontSize: "2.5rem",
+                fontWeight: 600,
+              }}
+            >
+              {selectedTab === TabEnum.Profile ? "Edit your account" : "Order history"}
+            </Box>
+            {selectedTab === TabEnum.Profile && (
+              <>
+                <ProfileForm
+                  isLoading={isLoading}
+                  allowEmptyPassword
+                  displayDeleteAccount
+                  submitButton={{
+                    onClick: () => handleSubmit(),
+                    content: "Save",
+                  }}
+                />
+                {!!errorMessage && <Alert severity="warning">{errorMessage}</Alert>}
+                {!!successMessage && <Alert severity="success">{successMessage}</Alert>}
+              </>
+            )}
+            {selectedTab === TabEnum.OrderHistory && <Invoices />}
+          </Stack>
         </Stack>
-      </Stack>
-    </Box>
+      </Box>
+    </>
   );
 };
 
