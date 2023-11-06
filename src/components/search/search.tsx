3867b457a19785b860bba0bc467567d4898e1a18
lisa_maz <lisa@shifters.me>
2023-11-06 03:07:44 +0100

    search bar

---
+++ src/components/search/search.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import IconButton from "@mui/material/IconButton";
+import SearchIcon from "@mui/icons-material/Search";
+import TextField from "@mui/material/TextField";
+
+interface SearchBarProps {
+  setSearchQuery: (query: string) => void;
+}
+
+const SearchBar: React.FC<SearchBarProps> = ({ setSearchQuery }) => (
+  <form>
+    <TextField
+      id="search-bar"
+      className="text"
+      onChange={(e) => {
+        setSearchQuery(e.target.value);
+      }}
+      label="Enter a city name"
+      variant="outlined"
+      placeholder="Search..."
+      size="small"
+    />
+    <IconButton type="submit" aria-label="search">
+      <SearchIcon style={{ fill: "blue" }} />
+    </IconButton>
+  </form>
+);
+
+export default SearchBar;
--- src/layouts/UsersLayout.tsx
+++ src/layouts/UsersLayout.tsx
@@ -6,6 +6,30 @@ import { request } from "../Config/request";
 import ListFiles from "../components/main/ListFiles";
 
 
+import SearchBar from "../components/search/search";
+
+const data = [
+  "Paris",
+  "London",
+  "New York",
+  "Tokyo",
+  "Berlin",
+  "Buenos Aires",
+  "Cairo",
+  "Canberra",
+  "Rio de Janeiro",
+  "Dublin",
+];
+
+const filterData = (query: string, data: any[]) => {
+  if (!query) {
+    return data;
+  } else {
+    return data.filter((d) => d.toLowerCase().includes(query.toLowerCase()));
+  }
+};
+
+
 const UsersLayout = () => {
   const [listItems, setListItems] = useState<dataItem[]>([])
 
@@ -18,13 +42,65 @@ const UsersLayout = () => {
       }, []
   )
 
-  return <Box sx={{height: "100vh", width: "100vw", display: "flex", flexDirection: "column", backgroundColor: "#05162F" }}>
+
+
+  const [searchQuery, setSearchQuery] = useState("");
+  const dataFiltered = filterData(searchQuery, data);
+
+  return (
+    <div className="layout-container">
+    
+    
+    <div className="page-container">
+    <div
+      style={{
+        display: "flex",
+        alignSelf: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+        padding: 20,
+      }}
+    >
+      <SearchBar setSearchQuery={setSearchQuery} />
+      <div style={{ padding: 3 }}>
+        {dataFiltered.map((d) => (
+          <div
+            className="text"
+            style={{
+              padding: 5,
+              justifyContent: "normal",
+              fontSize: 20,
+              color: "blue",
+              margin: 1,
+              width: "250px",
+              borderColor: "green",
+              borderWidth: "10px",
+            }}
+            key={d}
+          >
+            {d}
+          </div>
+        ))}
+      </div>
+    </div>
+    </div>
+    </div>
+  );
+
+
+
+
+  // return<>
+  // <Search/>
   
-    <Box sx={{flex: 1, display: "flex", width: "max"}}>
-      <ListFiles data={listItems} />
-     
-    </Box>
-  </Box>;
+  // <Box sx={{height: "100vh", width: "100vw", display: "flex", flexDirection: "column", backgroundColor: "#05162F" }}>
+  
+  //   <Box sx={{flex: 1, display: "flex", width: "max"}}>
+  //     <ListFiles data={listItems} />
+
+  //   </Box>
+  // </Box>;
+  // </>
 };
 
 export default UsersLayout;
